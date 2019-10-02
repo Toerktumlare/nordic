31a5962361b9b3bfab8edb52dc4e1560a57f9cff
Thomas Andolf <thomas.andolf@gmail.com>
2019-10-02 18:20:48 +0200

    added deployment configuration

--- src/main/java/se/andolf/nordic/config/SheetConfig.java
+++ src/main/java/se/andolf/nordic/config/SheetConfig.java
@@ -2,31 +2,45 @@ package se.andolf.nordic.config;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsRequestInitializer;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.http.HttpTransportFactory;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import se.andolf.nordic.config.properties.SheetConnectionProperties;
 import se.andolf.nordic.utils.FileUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 
 @Configuration
 public class SheetConfig {
 
+    private SheetConnectionProperties sheetConnectionProperties;
+
+    @Autowired
+    public SheetConfig(SheetConnectionProperties sheetConnectionProperties) {
+        this.sheetConnectionProperties = sheetConnectionProperties;
+    }
+
     @Bean
-    public Sheets sheets() {
+    public Sheets sheets() throws URISyntaxException {
         try {
-            final InputStream credentialStream = FileUtils.read("nordic-458436009a51.json");
+            final InputStream credentialStream = FileUtils.read(sheetConnectionProperties.getFilename());
             final GoogleCredentials credentials = ServiceAccountCredentials.fromStream(credentialStream).createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS_READONLY));
+
             final HttpRequestInitializer httpRequestInitializer = new HttpCredentialsAdapter(credentials);
             return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
                         JacksonFactory.getDefaultInstance(), httpRequestInitializer)
@@ -34,6 +48,13 @@ public class SheetConfig {
                         .build();
         } catch (GeneralSecurityException | IOException e) {
             throw new BeanCreationException("Could not create Sheets bean", e);
+        }
+    }
+
+    private class DefaultHttpTransportFactory implements HttpTransportFactory {
+
+        public HttpTransport create() {
+            return new NetHttpTransport();
         }
     }
 }
---
+++ src/main/java/se/andolf/nordic/config/properties/SheetConnectionProperties.java
@@ -0,0 +1,16 @@
+package se.andolf.nordic.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(
+        prefix = "google.sheets.credentials",
+        ignoreUnknownFields = false
+)
+@Data
+public class SheetConnectionProperties {
+
+    private String filename;
+}
