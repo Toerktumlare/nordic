9e991b3af40210a4032c508f23456b19ce52d0ef
Thomas Andolf <thomas.andolf@gmail.com>
2019-10-03 19:51:17 +0200

    refactored app component to be functional

--- frontend/nordic-monitoring/src/App.jsx
+++ frontend/nordic-monitoring/src/App.jsx
@@ -11,35 +11,21 @@ const body = {
   backgroundColor: '#737373',
 };
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  // componentDidMount() {
-  //   console.log('Fetching workouts');
-  //   const { fetchWorkouts } = this.props;
-  //   fetchWorkouts();
-  // }
-
-  render() {
-    return (
-      <div className="fl w-100 h-100" style={body}>
-        <div className="fl w-70 h-100">
-          <Workout />
-        </div>
-        <div className="fl w-30 h-100 pa1">
-          <div className="flex flex-column">
-            <Clock className="mb2" />
-            <InfoBar className="mb2" text="Dagens Pass - 07:00" />
-            <div className="mr2">
-              Attendee component
-            </div>
-          </div>
+const App = () => (
+  <div className="fl w-100 h-100" style={body}>
+    <div className="fl w-70 h-100">
+      <Workout />
+    </div>
+    <div className="fl w-30 h-100 pa1">
+      <div className="flex flex-column">
+        <Clock className="mb2" />
+        <InfoBar className="mb2" text="Dagens Pass - 07:00" />
+        <div className="mr2">
+          Attendee component
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default App;
