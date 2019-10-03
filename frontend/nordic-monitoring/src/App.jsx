63fd4e374804c221cef667e3a6ec10a6c0f2e957
Thomas Andolf <thomas.andolf@gmail.com>
2019-10-03 19:43:18 +0200

    connected Workout component to redux store

--- frontend/nordic-monitoring/src/App.jsx
+++ frontend/nordic-monitoring/src/App.jsx
@@ -1,13 +1,11 @@
+/* eslint-disable react/prefer-stateless-function */
 /* eslint-disable no-useless-constructor */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import PropTypes from 'prop-types';
 import './App.css';
-import { connect } from 'react-redux';
 import Clock from './components/clock/clock';
 import InfoBar from './components/infoBar/infoBar';
 import Workout from './components/workout/workout';
-import { getWorkouts } from './store/actions/workoutsActions';
 
 const body = {
   backgroundColor: '#737373',
@@ -18,19 +16,17 @@ class App extends React.Component {
     super(props);
   }
 
-  componentDidMount() {
-    console.log('Fetching workouts');
-    const { fetchWorkouts } = this.props;
-    fetchWorkouts();
-  }
+  // componentDidMount() {
+  //   console.log('Fetching workouts');
+  //   const { fetchWorkouts } = this.props;
+  //   fetchWorkouts();
+  // }
 
   render() {
-    const { wods } = this.props;
-    const filteredWorkouts = wods.filter((workoutType) => workoutType.name === 'DAGENS_PASS');
     return (
       <div className="fl w-100 h-100" style={body}>
         <div className="fl w-70 h-100">
-          <Workout workouts={filteredWorkouts} />
+          <Workout />
         </div>
         <div className="fl w-30 h-100 pa1">
           <div className="flex flex-column">
@@ -46,25 +42,4 @@ class App extends React.Component {
   }
 }
 
-App.propTypes = {
-  fetchWorkouts: PropTypes.func,
-};
-
-App.defaultProps = {
-  fetchWorkouts: () => {},
-};
-
-function mapStateToProps(state) {
-  return {
-    wods: state.wods,
-  };
-}
-
-const mapDispatchToProps = (dispatch) => ({
-  fetchWorkouts: () => dispatch(getWorkouts()),
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(App);
+export default App;
--- frontend/nordic-monitoring/src/components/workout/workout.jsx
+++ frontend/nordic-monitoring/src/components/workout/workout.jsx
@@ -1,104 +1,112 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import { getWorkouts } from '../../store/actions/workoutsActions';
 import WorkoutDay from './workoutDay';
 import Instructions from './instructions';
 import Header from './header';
 import WorkoutTypes from '../../constants/WorkoutTypes';
 import Footer from './footer';
 
-// const inlineStyle = {
-//   flex: '1'
-// };
-
 class Workout extends React.Component {
+  static genWorkoutDays = (weeklyWorkouts) => weeklyWorkouts.flatMap((workoutWeek) => {
+    const { name, workoutDays } = workoutWeek;
+    const workoutName = WorkoutTypes[name];
+
+    return workoutDays.map((workoutDay) => {
+      const { instructions, workouts, date } = workoutDay;
+      return (
+        <>
+          <div className="flex flex-column justify-between overflow-auto h-100">
+            <Header className="pl4" name={workoutName} timestamp={date} />
+            <WorkoutDay className="pl4 pr4" workouts={workouts} />
+            <Instructions className="pl4 pb2 pr4" values={instructions} />
+          </div>
+        </>
+      );
+    });
+  });
+
   constructor(props) {
     super(props);
 
-    const {
-      currentIndex,
-    } = this.props;
+    const { currentIndex, data } = this.props;
+    const workoutWeek = Workout.genWorkoutDays(data);
 
     this.state = {
       currentIndex,
+      workoutWeek,
     };
   }
 
-  // componentDidUpdate(prevProps) {
-  //   console.log('workout did update');
-  //   const { workouts } = this.props;
-  //   if (prevProps.workouts.length !== workouts.length) {
-  //     console.log('setting new workout');
-  //     // eslint-disable-next-line react/no-did-update-set-state
-  //     this.setState({
-  //       workoutDays: this.genWorkoutDays(workouts),
-  //     });
-  //   }
-  // }
+  componentDidMount() {
+    const { fetchWorkouts } = this.props;
+    fetchWorkouts();
+  }
 
-    handleForwardClick = (e) => {
-      e.preventDefault();
-      this.setState((prevState) => ({ currentIndex: prevState.currentIndex + 1 }));
+  static getDerivedStateFromProps(nextProps) {
+    return {
+      workoutWeek: Workout.genWorkoutDays(nextProps.data),
+    };
+  }
+
+  handleForwardClick = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => ({ currentIndex: prevState.currentIndex + 1 }));
+  }
+
+  handleBackwardClick = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => ({ currentIndex: prevState.currentIndex - 1 }));
+  }
+
+  render() {
+    const { className, currentIndex } = this.props;
+    const { workoutWeek } = this.state;
+    let selectedWorkoutDay;
+    if (workoutWeek.length !== 0) {
+      selectedWorkoutDay = workoutWeek[currentIndex];
     }
 
-    handleBackwardClick = (e) => {
-      e.preventDefault();
-      this.setState((prevState) => ({ currentIndex: prevState.currentIndex - 1 }));
-    }
-
-    genWorkoutDays = (weeklyWorkouts) => weeklyWorkouts.flatMap((workoutWeek) => {
-      const { name, workoutDays } = workoutWeek;
-      const workoutName = WorkoutTypes[name];
-
-      return workoutDays.map((workoutDay) => {
-        const { instructions, workouts, date } = workoutDay;
-        return (
-          <>
-            <div className="flex flex-column justify-between overflow-auto h-100">
-              <Header className="pl4" name={workoutName} timestamp={date} />
-              <WorkoutDay className="pl4 pr4" workouts={workouts} />
-              <Instructions className="pl4 pb2 pr4" values={instructions} />
-            </div>
-          </>
-        );
-      });
-    });
-
-    render() {
-      const { className, workouts, currentIndex } = this.props;
-      const workoutDays = this.genWorkoutDays(workouts);
-
-      let selectedWorkoutDay;
-      if (workoutDays.length !== 0) {
-        selectedWorkoutDay = workoutDays[currentIndex];
-      }
-
-      console.log('rendering workout');
-
-      return (
-        <div className={`bg-white flex flex-column justify-between ba bw3 b--yellow h-100 ${className}`}>
-          {selectedWorkoutDay}
-          <Footer
-            className="flex-0"
-            onForward={this.handleForwardClick}
-            onBackward={this.handleBackwardClick}
-          />
-        </div>
-      );
-    }
+    return (
+      <div className={`bg-white flex flex-column justify-between ba bw3 b--yellow h-100 ${className}`}>
+        {selectedWorkoutDay}
+        <Footer
+          className="flex-0"
+          onForward={this.handleForwardClick}
+          onBackward={this.handleBackwardClick}
+        />
+      </div>
+    );
+  }
 }
 
 Workout.propTypes = {
   className: PropTypes.string,
-  workouts: PropTypes.arrayOf(PropTypes.object),
+  data: PropTypes.arrayOf(PropTypes.object),
   currentIndex: PropTypes.number,
+  fetchWorkouts: PropTypes.func,
 };
-
 
 Workout.defaultProps = {
   className: '',
-  workouts: [],
+  data: [],
   currentIndex: moment().isoWeekday() - 1,
+  fetchWorkouts: () => {},
 };
 
-export default Workout;
+function mapStateToProps(state) {
+  return {
+    data: state.wods,
+  };
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchWorkouts: () => dispatch(getWorkouts()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Workout);
