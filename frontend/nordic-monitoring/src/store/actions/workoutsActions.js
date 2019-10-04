1d328f5caec40e68af095247098aca1dceebc716
Thomas Andolf <thomas.andolf@gmail.com>
2019-10-04 19:35:17 +0200

    disabled linting for console error statement

--- frontend/nordic-monitoring/src/store/actions/workoutsActions.js
+++ frontend/nordic-monitoring/src/store/actions/workoutsActions.js
@@ -15,6 +15,7 @@ export function getWorkouts() {
     .then(((json) => {
       dispatch(setWorkouts(json.data));
     })).catch((error) => {
+      // eslint-disable-next-line no-console
       console.error(error);
     });
 }
