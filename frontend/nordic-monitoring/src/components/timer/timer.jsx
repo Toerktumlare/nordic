878234a5412822a18279f01d2d57aff7aa00a711
Thomas Andolf <thomas.andolf@gmail.com>
2019-10-20 23:12:05 +0200

    make sure that timer stops at exactly 99:00

--- frontend/nordic-monitoring/src/components/timer/timer.jsx
+++ frontend/nordic-monitoring/src/components/timer/timer.jsx
@@ -29,7 +29,7 @@ const Timer = ({ className, style }) => {
     }
     setIntervalId(setInterval(() => {
       if (countRef.current === -1) setInCountdown(false);
-      if (countRef.current === 9) setRunning(false);
+      if (countRef.current === 5939) setRunning(false);
       setAbsTime(countRef.current + 1);
     }, 1000));
     setRunning(true);
