7c4128732a8ebc1c2c24a783a37a234a3efb5a59
Thomas Andolf <thomas.andolf@gmail.com>
2019-11-02 00:10:46 +0100

    fix strange poll bug

--- src/main/java/se/andolf/nordic/resources/workouts/FitnessResource.java
+++ src/main/java/se/andolf/nordic/resources/workouts/FitnessResource.java
@@ -1,5 +1,6 @@
 package se.andolf.nordic.resources.workouts;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import se.andolf.nordic.config.properties.WorkoutConfiguration;
 import se.andolf.nordic.resources.SheetResource;
 
@@ -7,5 +8,10 @@ public class FitnessResource extends AbstractWorkoutResource {
 
     public FitnessResource(WorkoutConfiguration workoutConfiguration, SheetResource sheetResource) {
         super(workoutConfiguration, sheetResource);
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    private void fetchSheet() {
+        get().doOnNext(sink::next).subscribe();
     }
 }
