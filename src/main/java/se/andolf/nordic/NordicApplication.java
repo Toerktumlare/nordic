c163cebf59e7e5652700cf329407186e9a40a4a9
Thomas Andolf <thomas.andolf@gmail.com>
2019-11-25 00:36:13 +0100

    print current locale and week

--- src/main/java/se/andolf/nordic/NordicApplication.java
+++ src/main/java/se/andolf/nordic/NordicApplication.java
@@ -1,5 +1,6 @@
 package se.andolf.nordic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,10 +11,12 @@ import java.util.Locale;
 @SpringBootApplication
 @EnableScheduling
 @EnableWebFlux
+@Slf4j
 public class NordicApplication {
 
 	public static void main(String[] args) {
         Locale.setDefault(new Locale("en", "GB"));
+        log.info("Default Locale: " + Locale.getDefault());
 		SpringApplication.run(NordicApplication.class, args);
 	}
 
